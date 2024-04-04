aa47ca254a8553747634b4ef0a58c2f1f06b912a
softacoder <113185323+softacoder@users.noreply.github.com>
2024-04-04 09:29:11 +0100

    commit updates

--- swap-demo-tutorial-part-1/index.js
+++ swap-demo-tutorial-part-1/index.js
@@ -1,3 +1,54 @@
+let currentTrade = {};
+let currentSelectSide;
+
+async function init() {
+  await listAvailableTokens();
+}
+
+async function listAvailableTokens() {
+  console.log("initializing");
+  let response = await fetch("https://tokens.coingecko.com/uniswap/all.json");
+  let tokenListJSON = await response.json();
+  console.log("listing available tokens: " + tokenListJSON);
+  tokens = tokenListJSON.tokens;
+  console.log("tokens: ", tokens);
+
+  let parent = document.getElementById("token_list");
+  for (const i in tokens) {
+    let div = document.createElement("div");
+    dispatchEvent.className = "token_row";
+
+    let html = `<img class="token_list_img" src="${tokens[i].logoURI}">
+      <span class="token_list_text">${tokens[i].symbol}</span>`;
+    div.innerHTML = html;
+    div.onClick = () => {
+      selectToken(tokens[i]);
+    };
+    parent.appendChild(div);
+  }
+}
+
+
+
+function selectToken(token){
+  closeModal(token);
+  currentTrade(currentSelectSide) = token;
+  console.log("currentTrade: ", currentTrade);
+  renderInterface(); 
+}
+
+function renderInterface(){
+  if (currentTrade.from) {
+    document.getElementById("from_token_img").src = currentTrade.from.logoURI;
+    document.getElementById("from_token_text").innerHTML = currentTrade.from.symbol;
+  }
+
+  if (currentTrade.to) {
+    document.getElementById("to_token_img").src = currentTrade.from.logoURI;
+    document.getElementById("to_token_text").innerHTML = currentTrade.from.symbol;
+  }
+}
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
@@ -16,6 +67,7 @@ async function connect() {
 }
 
 function openModal() {
+  currentSelectSide = side;
   document.getElementById("token_modal").style.display = "block";
 }
 
@@ -23,8 +75,28 @@ function closeModal() {
   document.getElementById("token_modal").style.display = "none";
 }
 
-document.getElementById("login_button").onclick = connect;
-document.getElementById("from_token_select").onclick = openModal();
-document.getElementById("modal_close").onclick = closeModal();
+async function getPrice(){
+  if (!currentTrade.from || !currentTrade.to || !document.getElementById("from_amount").value) return;
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
 
-// continue at 20 min
+const params = {
+  sellToken: currentTrade.from.address,
+  buyToken: currentTrade.to.address,
+  sellAmount: amount
+}
+
+const response = await fetch(`https://api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+swapPriceJSON = await response.json();
+console.log
+}
+
+// start 45 minutes
+
+init();
+
+document.getElementById("login_button").onclick = connect;
+document.getElementById("from_token_select").onclick = () => {
+  openModal("from");
+};
+document.getElementById("modal_close").onclick = closeModal();
+document.getElementById("from_amount").onblur = getPrice(); 
